3ea03123917891c7da31d8a36f914132d333d2a7
J Cole Morrison <j.cole.morrison@gmail.com>
2017-02-25 23:00:56 -0800

    use previous value

--- cfn/build.sh
+++ cfn/build.sh
@@ -11,4 +11,12 @@ echo "Pushing image"
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$DATE_TAG
 echo "Updating CFN"
 aws cloudformation update-stack --stack-name autoscalae --use-previous-template \
-  --parameters ParameterKey=WebTaskImage,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$DATE_TAG
+  --parameters ParameterKey=WebTaskImage,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$DATE_TAG \
+  ParameterKey=AutoScale,UsePreviousValue=true \
+  ParameterKey=BastionSecurityGroup,UsePreviousValue=true \
+  ParameterKey=DesiredCapacity,UsePreviousValue=true \
+  ParameterKey=InstanceType,UsePreviousValue=true \
+  ParameterKey=KeyPairName,UsePreviousValue=true \
+  ParameterKey=MaxSize,UsePreviousValue=true \
+  ParameterKey=SubnetID,UsePreviousValue=true \
+  ParameterKey=VpcId,UsePreviousValue=true
