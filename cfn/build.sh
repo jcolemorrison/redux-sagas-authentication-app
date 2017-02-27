c9bceda989dc1059e23eeb13be361443c2b499ad
J Cole Morrison <j.cole.morrison@gmail.com>
2017-02-26 16:32:34 -0800

    remove usage of tpls

--- cfn/build.sh
+++ cfn/build.sh
@@ -5,8 +5,6 @@ DATE_TAG=`date +%s%3N`
 
 echo "Building image..."
 docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$DATE_TAG .
-echo "Creating configuration..."
-node ./cfn/create-configure.js
 echo "Pushing image"
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$DATE_TAG
 echo "Updating CFN"
